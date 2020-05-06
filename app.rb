ffc8e974c1d525640625ef21770911e9070f7a53
Nicholas Keller <nic.keller328@gmail.com>
2020-05-06 11:38:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,4 +32,24 @@ class App < Sinatra::Base
 
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+
+    if @operation == 'add'
+      (@number1 + @number2).to_s
+    elsif @operation == 'subtract'
+      (@number1 - @number2).to_s
+    elsif @operation == 'multiply'
+      (@number1 * @number2).to_s
+    elsif @operation == 'divide'
+      (@number1 / @number2).to_s
+    end
+  end
+  end
+
+
+
 end
